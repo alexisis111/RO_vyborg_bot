3753afc61d5548307b428c9480d1094ab097521e
alexisis111 <shuragilyow1@gmail.com>
2024-09-19 22:47:18 +0300

    тест

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
             queryId,
             userId
         };
-        fetch('https://768e-176-59-10-183.ngrok-free.app/web-data', {
+        fetch('https://1171-185-108-19-43.ngrok-free.app/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
